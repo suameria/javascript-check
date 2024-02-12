b1ede9bea82d8ca6dc2e1cc19855950b53d151af
Hiroshi.Arai <suameria@gmail.com>
2024-02-12 13:47:16 +0900

    create chat app

--- app/Chat/app.js
+++ app/Chat/app.js
@@ -1,44 +1,75 @@
 import users from "./users.json" assert { type: "json" };
 
 (() => {
-  "use strict";
+  ("use strict");
 
   const loginUserText = document.getElementById("loginUserText");
   const selectUser = document.getElementById("selectUser");
+  const postCommentBtn = document.getElementById("postCommentBtn");
+  const postComment = document.getElementById("postComment");
+  const chatLists = document.getElementById("chatLists");
+  let listTemplate = document.getElementsByClassName("list-template")[0];
+  let userImgSrc = "";
 
-  const chat = {
-    lists: [
-      {
-        user_img_src: "https://source.unsplash.com/100x100/?portrait?2",
-        post_comment: "Hi 3",
-        post_time: "3 hours ago",
-      },
-      {
-        user_img_src: "https://source.unsplash.com/100x100/?portrait?1",
-        post_comment: "Hi 2",
-        post_time: "2 hours ago",
-      },
-      {
-        user_img_src: "https://source.unsplash.com/100x100/?portrait?0",
-        post_comment: "Hi 1",
-        post_time: "1 hours ago",
-      },
-    ],
+  const getDateNow = () => {
+    const date = new Date();
+    date.setTime(date.getTime() - date.getTimezoneOffset() * 60 * 1000);
+    const str_date = date.toISOString().replace("T", " ").substring(0, 19);
+    return str_date;
   };
 
-  const listTemplate = document.getElementById("list-0");
+  const getChatListsByLocalStorage = () => {
+    const chat = JSON.parse(localStorage.getItem("chat-lists"));
+    return chat ?? { lists: [] };
+  };
 
-  chat.lists.forEach((list) => {
-    const cloneListTemplate = listTemplate.cloneNode(true);
-    cloneListTemplate.id = "list-1";
-    cloneListTemplate.style.display = "block";
-    cloneListTemplate.querySelector("img").src = list.user_img_src;
-    cloneListTemplate.querySelector("#post_comment-0").innerText =
-      list.post_comment;
-    cloneListTemplate.querySelector("#post_time-0").innerText = list.post_time;
+  const storeChatListsByLocalStorage = (chatLists) => {
+    localStorage.setItem("chat-lists", JSON.stringify(chatLists));
+  };
 
-    listTemplate.after(cloneListTemplate);
-  });
+  // const deleteAllChatListsByLocalStorage = () => {
+  //   localStorage.clear();
+  // };
+  // deleteAllChatListsByLocalStorage();
+
+  const renderChatLists = (listTemplate) => {
+    const chat = getChatListsByLocalStorage();
+    if (!chat.lists.length) {
+      return;
+    }
+    chat.lists.forEach((list) => {
+      const cloneListTemplate = listTemplate.cloneNode(true);
+      cloneListTemplate.style.display = "block";
+      cloneListTemplate.querySelector("img").src = list.user_img_src;
+      cloneListTemplate.querySelector("#post_comment-0").innerText =
+        list.post_comment;
+      cloneListTemplate.querySelector("#post_time-0").innerText =
+        list.post_time;
+
+      listTemplate.after(cloneListTemplate);
+    });
+  };
+
+  const resetRenderChatLists = () => {
+    const lists = document.getElementsByClassName("list-template");
+    listTemplate = lists.item(0);
+    while (lists.length) {
+      lists.item(0).remove();
+    }
+    chatLists.appendChild(listTemplate);
+
+    return listTemplate;
+  };
+
+  const realTimeRender = () => {
+    listTemplate = resetRenderChatLists();
+    renderChatLists(listTemplate);
+  };
+
+  renderChatLists(listTemplate);
+
+  // 1秒ごとにチャットリストを更新
+  setInterval(realTimeRender, 1000);
 
   selectUser.addEventListener("click", (e) => {
     users.login.name = e.target.dataset.name;
@@ -47,6 +78,39 @@ import users from "./users.json" assert { type: "json" };
       return;
     }
 
+    userImgSrc = e.target.src;
+    loginUserText.style.display = "block";
     loginUserText.innerText = `${users.login.name}でログイン中`;
+  });
+
+  postCommentBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (!userImgSrc) {
+      loginUserText.style.display = "block";
+      loginUserText.innerText = "ログインしてください";
+      return;
+    }
+
+    const comment = postComment.value;
+    if (!comment) {
+      loginUserText.innerText = "コメントを入力してください";
+      return;
+    }
+
+    const chat = getChatListsByLocalStorage();
+
+    chat.lists.push({
+      user_img_src: userImgSrc,
+      post_comment: comment,
+      post_time: getDateNow(),
+    });
+
+    storeChatListsByLocalStorage(chat);
+
+    postComment.value = "";
+
+    listTemplate = resetRenderChatLists();
+    renderChatLists(listTemplate);
   });
 })();
